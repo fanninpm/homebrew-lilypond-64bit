6b282892ae4353e3ef1a2211cd2e7166df634bb7
Martin Schimandl <martin.schimandl@gmail.com>
2017-07-15 10:19:35 +0200

    Add appcast to gridcoin cask

--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -3,6 +3,8 @@ cask 'gridcoin' do
   sha256 'a7706c3538dbf6852366befda0c3d5bd170970996c0f1334c7ce6cf3c4546344'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/#{version}/gridcoinresearch.dmg"
+  appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom',
+          checkpoint: '3b3ada7d9b0288e898b20f4ce675b3225a15060f29ac99b4676923f4c433d5c9' 
   name 'Gridcoin macOS Wallet'
   homepage 'https://gridcoin.us/'
 
