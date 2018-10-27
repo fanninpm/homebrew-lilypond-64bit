197a212b0fc629ee714a70480fec212dd189ab94
scribblemaniac <scribblemaniac@users.noreply.github.com>
2018-10-26 23:16:47 -0600

    Add TupiTube.app 0.2.11 (#32)

---
+++ Casks/tupitube.rb
@@ -0,0 +1,12 @@
+cask 'tupitube' do
+  version '0.2.11'
+  sha256 'c31f859081746c75e867b16bbedb6118e8c774a6c931852855094c40126598a1'
+
+  url "https://downloads.sourceforge.net/tupi2d/tupitube_desk_#{version}.dmg"
+  appcast 'https://sourceforge.net/projects/tupi2d/rss?path=/Mac%20OSX'
+  name 'TupiTube Desk'
+  name 'Tupi2d'
+  homepage 'https://www.maefloresta.com/'
+
+  app 'TupiTube.app'
+end
