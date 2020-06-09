61d363d2eab8732f1bccf9dfb6b64cc54275f7c8
Philippe Nénert (@papilip) <me@papilip.me>
2020-06-09 11:11:23 +0200

    Change version number : add comma and build number

--- Casks/lilypond-unofficial.rb
+++ Casks/lilypond-unofficial.rb
@@ -1,9 +1,9 @@
 cask 'lilypond-unofficial' do
-  version '2.20.0'
+  version '2.20.0,20200311175017'
   sha256 '655bb00970279ae72d3c7dfe19186db9025f0cb5eae7e3c4485c1b3b1116bc69'
 
   # bintray.com/marnen/lilypond-darwin-64/download_file?file_path=lilypond- was verified as official when first introduced to the cask
-  url "https://bintray.com/marnen/lilypond-darwin-64/download_file?file_path=lilypond-#{version}.build20200311175017-darwin-64.tar.gz"
+  url "https://bintray.com/marnen/lilypond-darwin-64/download_file?file_path=lilypond-#{version.before_comma}.build#{version.after_comma}-darwin-64.tar.gz"
   appcast 'https://bintray.com/marnen/lilypond-darwin-64/lilypond-2.20.0#release'
   name 'LilyPond'
   homepage 'https://lilypond.org/'
